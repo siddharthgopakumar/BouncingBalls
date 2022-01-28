36c209dad346b75f937ac2049b8e279910713309
Siddharth Gopakumar <siddharthgopi@outlook.com>
2022-01-28 13:28:06 +0530

    Bouncing balls wihtout collision detect

--- main.js
+++ main.js
@@ -12,3 +12,67 @@ function random(min, max) {
   const num = Math.floor(Math.random() * (max - min + 1)) + min;
   return num;
 }
+function randomRGB()    {
+    return `rgb(${random(0,255)},${random(0,255)},${random(0,255)})`;
+}
+class Ball {
+    constructor(x, y, velX, velY, color, size)  {
+        this.x = x;
+        this.y = y;
+        this.velX = velX;
+        this.velY = velY;
+        this.color = color;
+        this.size = size;
+    }
+    draw() {
+        ctx.beginPath();
+        ctx.fillStyle = this.color;
+        ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+        ctx.fill();
+    }
+    update() {
+        if((this.x + this.size) >= width)   {
+            this.velX = -(this.velX);
+        }
+        if((this.x - this.size) <= 0)   {
+            this.velX = -(this.velX);
+        }
+        if((this.y + this.size) >= height)   {
+            this.velY = -(this.velY);
+        }
+        if((this.y - this.size) <= 0)   {
+            this.velY = -(this.velY);
+        }
+        this.x += this.velX;
+        this.y += this.velY;
+    }
+}
+
+const balls = [];
+
+while(balls.length < 20)    {
+    const size = random(10, 20);
+    const ball = new Ball(
+        random(0 + size, width - size),
+        random(0 + size, height - size),
+        random(-7,7),
+        random(-7,7),
+        randomRGB(),
+        size
+    ); 
+    balls.push(ball);
+}
+function loop() {
+    ctx.fillStyle = 'coral';
+    ctx.fillRect(0,0,width, height);
+
+    for(const ball of balls)    {
+        ball.draw();
+        ball.update();
+      //  ball.collisionDetect();
+    }
+
+    requestAnimationFrame(loop);
+}
+
+loop();
